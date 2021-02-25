d19d7391caecdbfca31837217a0dead1913ece29
Daniela Opitz <dan.opitz@gmail.com>
2021-02-25 11:41:34 -0300

    files

---
+++ clase01/03_strings.py
@@ -0,0 +1,6 @@
+primero = 'Daniela '
+segundo = 'Opitz '
+tercero = 'Gonzalez'
+nombre = primero + segundo + tercero
+print('Tu nombre es', primero, segundo, tercero)
+
---
+++ clase01/04_enteros.py
@@ -0,0 +1,6 @@
+nombre = 'Daniela'
+edad = '35'
+edad2= int(edad)
+print('Hola', nombre, 'veo que tienes', str(edad2), 'años')
+decenas = edad2 // 10
+print('Y al menos tienes', decenas, 'decadas')
